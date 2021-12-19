47e9945ecf952e12345b7002f531b5f337746446
Paulo Valarezo <paulinho.ec.1@gmail.com>
2021-12-18 21:23:38 -0500

    Creación rama Paulo

---
+++ app/src/main/java/com/pst2021/amst4/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.pst2021.amst4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
