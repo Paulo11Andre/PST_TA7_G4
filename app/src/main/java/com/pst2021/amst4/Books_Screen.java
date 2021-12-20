e4240554be36fbaa29ed314ad84ad24561ca6664
Paulo Valarezo <paulinho.ec.1@gmail.com>
2021-12-20 01:55:03 -0500

    adición de activities categorías, libros, perfil

---
+++ app/src/main/java/com/pst2021/amst4/Books_Screen.java
@@ -0,0 +1,14 @@
+package com.pst2021.amst4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Books_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books__screen);
+    }
+}
