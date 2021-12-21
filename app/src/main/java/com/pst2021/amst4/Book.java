d9a363f3b77aa68d4e69b8f19bc18c194651758d
Dffranco27 <dffranco@espol.edu.ec>
2021-12-20 19:09:05 -0500

    Constructores, setters and getters de las clases Book y User.

--- app/src/main/java/com/pst2021/amst4/Book.java
+++ app/src/main/java/com/pst2021/amst4/Book.java
@@ -1,7 +1,78 @@
 package com.pst2021.amst4;
 
 public class Book {
-    public String title;
-    public String categoria;
-    public int anio;
+    private String titulo;
+    private Categoria categoria;
+    private int anio;
+    private String autor;
+    private String descripcion;
+    private String editorial;
+    private String imagen;
+
+    public Book(String titulo,String descripcion, String autor, String editorial, int anio,String imagen, Categoria categoria){
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.autor = autor;
+        this.editorial= editorial;
+        this.anio = anio;
+        this.imagen = imagen;
+        this.categoria= categoria;
+
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
--- app/src/main/java/com/pst2021/amst4/User.java
+++ app/src/main/java/com/pst2021/amst4/User.java
@@ -2,11 +2,99 @@ package com.pst2021.amst4;
 import com.pst2021.amst4.Categoria;
 
 public class User {
-    public String nombre;
-    public String apellido;
-    public String categoria;
-    public String correo;
-    public double telefonfo;
-    public String Username;
-    public String password;
+    private String nombre;
+    private String apellido;
+    private Categoria categoriaFav;
+    private String correo;
+    private double telefono;
+    private String Username;
+    private String password;
+    private String imagenPerfil;
+
+    public User(String nombre, String apellido, Categoria categoriaFav, String correo, double telefonfo, String username, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.categoriaFav = categoriaFav;
+        this.correo = correo;
+        this.telefono = telefono;
+        Username = username;
+        this.password = password;
+        this.imagenPerfil = "default";
+
     }
+
+    public User(String nombre, String apellido, Categoria categoriaFav, String correo, double telefono, String username, String password, String imagenPerfil) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.categoriaFav = categoriaFav;
+        this.correo = correo;
+        this.telefono = telefono;
+        Username = username;
+        this.password = password;
+        this.imagenPerfil = imagenPerfil;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Categoria getCategoriaFav() {
+        return categoriaFav;
+    }
+
+    public void setCategoriaFav(Categoria categoriaFav) {
+        this.categoriaFav = categoriaFav;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImagenPerfil() {
+        return imagenPerfil;
+    }
+
+    public void setImagenPerfil(String imagenPerfil) {
+        this.imagenPerfil = imagenPerfil;
+    }
+}
